b69bd03e958775dc81b31a82a9e50c523d5f4171
Matheus Emanoel <buss.matheusemanoel@gmail.com>
2025-02-11 03:30:43 -0300

    feat: add types and constants to project

---
+++ types/index.d.ts
@@ -0,0 +1,54 @@
+declare type SearchParamProps = {
+  params: Promise<{ [key: string]: string }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+
+declare type Gender = "Male" | "Female" | "Other";
+declare type Status = "pending" | "scheduled" | "canceled";
+
+declare interface CreateUserParams {
+  name: string;
+  email: string;
+  phone: string;
+}
+declare interface User extends CreateUserParams {
+  $id: string;
+}
+
+declare interface RegisterUserParams extends CreateUserParams {
+  userId: string;
+  birthDate: Date;
+  gender: Gender;
+  address: string;
+  occupation: string;
+  emergencyContactName: string;
+  emergencyContactNumber: string;
+  primaryPhysician: string;
+  insuranceProvider: string;
+  insurancePolicyNumber: string;
+  allergies?: string;
+  currentMedication?: string;
+  familyMedicalHistory?: string;
+  pastMedicalHistory?: string;
+  identificationType?: string;
+  identificationNumber?: string;
+  identificationDocument?: FormData;
+  privacyConsent: boolean;
+}
+
+declare type CreateAppointmentParams = {
+  userId: string;
+  patient: string;
+  primaryPhysician: string;
+  reason: string;
+  schedule: Date;
+  status: Status;
+  note: string | undefined;
+};
+
+declare type UpdateAppointmentParams = {
+  appointmentId: string;
+  userId: string;
+  appointment: Appointment;
+  type: string;
+};

f3cf5c80e9948afee8596f71979293ae6c55f689
Matheus Emanoel <buss.matheusemanoel@gmail.com>
2025-02-11 03:29:23 -0300

    feat: add actions to patient

---
+++ lib/actions/patient.actions.ts
@@ -0,0 +1,104 @@
+"use server";
+import { ID, Query } from "node-appwrite";
+import {
+  BUCKET_ID,
+  database,
+  DATABASE_ID,
+  ENDPOINT,
+  PATIENT_COLLECTION_ID,
+  PROJECT_ID,
+  storage,
+  users,
+} from "../appwrite.config";
+import { parseStringify } from "@/app/lib/utills";
+import { InputFile } from "node-appwrite/file";
+
+export const createUser = async (user: CreateUserParams) => {
+  try {
+    const newUser = await users.create(
+      ID.unique(),
+      user.email,
+      user.phone,
+      undefined,
+      user.name
+    );
+
+    console.log(newUser);
+    return parseStringify(newUser) as User;
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    // Check existing user
+    console.log(error);
+    if (error && error?.code === 409) {
+      const existingUser = await users.list([
+        Query.equal("email", [user.email]),
+      ]);
+
+      return parseStringify(existingUser.users[0]);
+    }
+    console.error("An error occurred while creating a new user:", error);
+  }
+};
+
+export const getUser = async (userId: string) => {
+  try {
+    const user = await users.get(userId);
+
+    return parseStringify(user);
+  } catch (error) {
+    console.error("An error occurred while retrieving the user: ", error);
+  }
+};
+
+export const registerPatient = async ({
+  identificationDocument,
+  ...patient
+}: RegisterUserParams) => {
+  try {
+    let file;
+
+    if (identificationDocument) {
+      const inputFile = InputFile.fromBuffer(
+        identificationDocument?.get("blobFile") as Blob,
+        identificationDocument?.get("fileName") as string
+      );
+
+      file = await storage.createFile(BUCKET_ID!, ID.unique(), inputFile);
+    }
+
+    const genderLowerCase = patient.gender.toLowerCase();
+    const newPatient = await database.createDocument(
+      DATABASE_ID!,
+      PATIENT_COLLECTION_ID!,
+      ID.unique(),
+      {
+        ...patient,
+        identificationDocumentId: file?.$id || null,
+        identificationDocumentUrl: `${ENDPOINT}/storage/buckets/${BUCKET_ID}/files/${file?.$id}/view?project=${PROJECT_ID}`,
+        gender: genderLowerCase,
+      }
+    );
+
+    return parseStringify(newPatient);
+  } catch (error) {
+    console.error("An error occurred while registering the patient: ", error);
+  }
+};
+
+export const getPatient = async (userId: string) => {
+  try {
+    const patients = await database.listDocuments(
+      DATABASE_ID!,
+      PATIENT_COLLECTION_ID!,
+      [Query.equal("userId", [userId])]
+    );
+
+    return parseStringify(patients.documents[0]);
+  } catch (error) {
+    console.error(
+      "An error occurred while retrieving the patient details: ",
+      error
+    );
+  }
+};

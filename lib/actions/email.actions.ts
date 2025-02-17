1b7730605e50d8b28dea44a503f5a329ebe0ce03
Matheus Emanoel <buss.matheusemanoel@gmail.com>
2025-02-17 07:54:12 -0300

    feat: create new email sender to send service status

--- lib/actions/email.actions.ts
+++ lib/actions/email.actions.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import type { Status } from "@/types";
 import { MailerSend, EmailParams, Recipient } from "mailersend";
 const apiKey = process.env.EMAIL_API_KEY!;
 
@@ -7,7 +8,45 @@ const mailersend = new MailerSend({
   apiKey,
 });
 
-export const sendEmail = async (
+export const sendEmailWithServiceStats = async (
+  toEmail: string,
+  name: string,
+  status: Status,
+  serviceName: string,
+  note?: string
+) => {
+  const senders = [new Recipient(toEmail, name)];
+  const variables = [
+    {
+      email: senders[0].email,
+      data: {
+        name,
+        note: note || "",
+        status,
+        serviceName,
+      },
+    },
+  ];
+
+  const subject = `Your service has been ${status}!`;
+  const emailParams = new EmailParams()
+
+    .setFrom({
+      email: "drivesafe@trial-7dnvo4dqqq3l5r86.mlsender.net",
+      name: "DriveSafe",
+    })
+    .setSubject(subject)
+    .setTemplateId("3yxj6lj5vq7gdo2r")
+    .setTo(senders)
+    .setPersonalization(variables);
+
+  try {
+    mailersend.email.send(emailParams);
+  } catch (error) {
+    console.error("An error occurred while trying to send email: ", error);
+  }
+};
+
 export const sendEmailVerification = async (
   toEmail: string,
   name: string,

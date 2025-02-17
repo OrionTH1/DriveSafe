566369636482f8da27a0e02fb1f0a02dc59eefc8
Matheus Emanoel <buss.matheusemanoel@gmail.com>
2025-02-17 07:52:04 -0300

    styls: add pading top to RequestService Page

--- app/(private)/services/request/page.tsx
+++ app/(private)/services/request/page.tsx
@@ -1,7 +1,7 @@
 import ServiceForm from "@/components/forms/ServiceForm";
 async function RequestService() {
   return (
-    <div className="w-full mx-auto flex max-w-7xl flex-col space-y-14 ">
+    <div className="w-full pt-8 mx-auto flex max-w-7xl flex-col space-y-14 ">
       <main className="">
         <ServiceForm type="create" />
       </main>

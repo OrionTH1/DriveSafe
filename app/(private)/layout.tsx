9f6df0ad96056d3c3867fe765ffbc06e92aff2e3
Matheus Emanoel <buss.matheusemanoel@gmail.com>
2025-02-21 02:13:56 -0300

    fix: layout wand mounting two html tag

--- app/(private)/layout.tsx
+++ app/(private)/layout.tsx
@@ -2,7 +2,6 @@ import "../globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Image from "next/image";
 import AccountDropdown from "@/components/AccountDropdown";
-import { cn } from "@/lib/utils";
 import Link from "next/link";
 
 export default function RootLayout({
@@ -11,30 +10,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={cn("min-h-screen bg-dark-300 antialiased")}>
-        <header className="admin-header ">
-          <Link href={"/"}>
-            <Image
-              src="/assets/icons/logo-full.svg"
-              width={162}
-              height={32}
-              alt="Logo"
-              className="h-8 w-fit"
-            />
-          </Link>
+    <div>
+      <header className="admin-header ">
+        <Link href={"/"}>
+          <Image
+            src="/assets/icons/logo-full.svg"
+            width={162}
+            height={32}
+            alt="Logo"
+            className="h-8 w-fit"
+          />
+        </Link>
 
-          <AccountDropdown />
-        </header>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
+        <AccountDropdown />
+      </header>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </div>
   );
 }
